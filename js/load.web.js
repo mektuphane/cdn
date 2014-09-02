2e3d6a00e6da468a51a2baed98424ee0a0f95354
Emrah SARI <emrahsari@msn.com>
2014-09-02 15:55:43 +0300

    no message

--- js/load.web.js
+++ js/load.web.js
@@ -21,17 +21,12 @@ function loadScripts(array,callback){
 
 loadScripts([
 	"http://cdn.mektuphane.com/js/jquery-1.11.1.min.js",
-	// "http://localhost:8888/cdn/js/bootstrap.min.js",
+	// "http://cdn.mektuphane.com/js/bootstrap.min.js",
     "//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js",
     "http://cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/js/jquery.Jcrop.min.js",
-    // "//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js",
-    // "http://localhost:8888/cdn/js/bootstrap-formhelpers.min.js",
-    // "//cdnjs.cloudflare.com/ajax/libs/bootcards/0.1.0/js/bootcards.min.js",
     "http://cdn.mektuphane.com/js/bootstrap-multiselect.js",
-    // "http://davidstutz.github.io/bootstrap-multiselect/js/bootstrap-multiselect.js",
     "http://cdn.mektuphane.com/js/remoteNonStopPageScroll.js",
     "http://cdn.mektuphane.com/js/bootstrap-maxlength.min.js",
-    "http://cdn.mektuphane.com/js/holder.js",
     "http://cdn.mektuphane.com/js/attach.web.js",
     "http://cdn.mektuphane.com/js/main.web.js"
 ],function(){
