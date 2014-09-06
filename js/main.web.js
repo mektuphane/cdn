c61ffeb8b76a575dfe52570009e216845854f5fc
Emrah SARI <emrahsari@msn.com>
2014-09-06 23:08:16 +0300

    no message

--- js/main.web.js
+++ js/main.web.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
 (function($){ 
     $(window).load(function(){
-        $("[id^='list-group_']").mCustomScrollbar({setHeight:350,scrollbarPosition:"outside",autoHideScrollbar:true});
+        $("[id^='list-group_']").mCustomScrollbar({setHeight:300,autoHideScrollbar:true});
     });
  })(jQuery);
 
