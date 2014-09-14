69a9dfe05756564e020c82ecd3b7c86d1d57d1ec
Emrah SARI <emrahsari@msn.com>
2014-09-14 09:25:19 +0300

    no message

--- js/attach.web.js
+++ js/attach.web.js
@@ -2,8 +2,8 @@
 
   $.fn.simpleCropper = function() {
 
-    var image_dimension_x = 1056 / 2;
-    var image_dimension_y = image_dimension_x / 1.41 / 2;
+    var image_dimension_x = 1056 / 1.1;
+    var image_dimension_y = image_dimension_x / 1.41 / 1.1;
     var scaled_width = 0;
     var scaled_height = 0;
     var x1 = 0;
@@ -145,7 +145,7 @@
               bgColor: '#747474',
               bgOpacity: .4,
               aspectRatio: aspX / aspY,
-              setSelect: [0, 0, selection_width * 2, selection_height * 2]
+              setSelect: [0, 0, selection_width * 1.1, selection_height * 1.1]
             }, function() {
               jcrop_api = this;
             });
