e58cc0bc4df2e77b5243bb22990499922d931845
Emrah SARI <emrahsari@msn.com>
2014-09-14 11:38:58 +0300

    no message

--- js/attach.web.js
+++ js/attach.web.js
@@ -213,8 +213,8 @@
     	var photo = file_display_area.attr('id');
     	console.log(photo);
     	$.ajax({
-    		// url: 'http://web.mektuphane.com/post/upload',
-        url: 'http://localhost:8080/post/upload',
+    		url: 'http://web.mektuphane.com/post/upload',
+        // url: 'http://localhost:8080/post/upload',
     		dataType: 'text',
     		data: {photo:image64},
     		type: 'POST',
