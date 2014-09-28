657a7713619b40ba0ea4336eab5e08cee08cf514
Emrah SARI <emrahsari@msn.com>
2014-09-28 10:46:59 +0300

    no message

---
+++ js/load.ios.js
@@ -0,0 +1,37 @@
+function loadScripts(array,callback){
+    var loader = function(src,handler){
+        var script = document.createElement("script");
+        script.src = src;
+        script.onload = script.onreadystatechange = function(){
+        script.onreadystatechange = script.onload = null;
+        	handler();
+        }
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+        //(head || document.body).appendChild( script );
+    };
+    (function(){
+        if(array.length!=0){
+        	loader(array.shift(),arguments.callee);
+        }else{
+        	callback && callback();
+        }
+    })();
+}
+
+loadScripts([
+	"http://cdn.mektuphane.com/js/jquery-1.11.1.min.js",
+	"http://cdn.mektuphane.com/js/bootstrap.min.js",
+    "//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js",
+    "http://cdn.mektuphane.com/js/jquery.Jcrop.min.js",
+    "http://cdn.mektuphane.com/js/bootstrap-multiselect.js",
+    "http://cdn.mektuphane.com/js/remoteNonStopPageScroll.js",
+    "http://cdn.mektuphane.com/js/bootstrap-maxlength.min.js",
+    "http://cdn.mektuphane.com/js/jquery.backstretch.min.js",
+    "http://cdn.mektuphane.com/js/jquery.mCustomScrollbar.concat.min.js",
+    "http://cdn.mektuphane.com/js/ion.sound.js",
+    "http://cdn.mektuphane.com/js/attach.web.js",
+    "http://cdn.mektuphane.com/js/main.ios.js"
+],function(){
+    console.info('LOADED');
+});
